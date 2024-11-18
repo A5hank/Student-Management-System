b64f855eefc0a09190b21e88d6a774569199e4b2
A5hank <ashankkumar84@gmail.com>
2024-11-18 12:55:11 +0530

    3/5 functions made

--- main.c
+++ main.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-
-#define MAX_STUDENTS 100
 
 void Menu() {
     printf("\n==== University Student Management ====\n");
     printf("1. Add Student Details\n");
     printf("2. Find Student by Roll Number\n");
-    printf("3. Find ICP Marks of a Student\n");
-    printf("4. Count the Number of Students\n");
-    printf("5. Delete a Student\n");
-    printf("6. Update a Student Entry\n");
-    printf("7. Exit\n");
+    printf("3. Count the Number of Students\n");
+    printf("4. Delete a Student\n");
+    printf("5. Update a Student Entry\n");
+    printf("6. Exit\n");
 }
-
-typedef struct /*Main Struucture to store data of students*/
+typedef struct student /*Main Struucture to store data of students*/
 {
     char name[100];
     int rollno;
@@ -23,24 +20,62 @@ typedef struct /*Main Struucture to store data of students*/
     int marks;
 } student;
 
-student students[MAX_STUDENTS];
-int studentCount=0;
+student students[100];
+int studentcount=0;
 
-
-void adddata(char name[],int rollno,char address[],int phoneno,int marks)
+void adddata()
 {
-    student s1;
-    strcpy(s1.name,name);
-    s1.rollno=rollno;
-    strcpy(s1.address,address);
-    s1.phoneno=phoneno;
-    s1.marks=marks;
+    printf("Enter Student's Name: ");
+    scanf(" %s", students[studentcount].name);
+    printf("Enter Student's Roll Number: ");
+    scanf("%d", &students[studentcount].rollno);
+    printf("Enter Student's Address: ");
+    scanf(" %[^\n]s", students[studentcount].address);
+    printf("Enter Student's Phone Number: ");
+    scanf("%d", &students[studentcount].phoneno);
+    printf("Enter Student's Marks: ");
+    scanf("%d", &students[studentcount].marks);
+    studentcount++;
+    printf("Student added successfully!\n");
 }
 
+void finddata(){
+    int x;
+    printf("Enter Roll Number of Student you want to find: ");
+    scanf("%d",&x);
+    for(int i = 0;i<studentcount;i++){
+        if(x==students[i].rollno){
+            printf("Name :%s\n",students[i].name);
+            printf("Roll Number: %d\n",students[i].rollno);
+            printf("Address: %s\n",students[i].address);
+            printf("Phone Number: %d\n",students[i].phoneno);
+            printf("ICP Marks: %d\n",students[i].marks);
+        }
+        else{
+            printf("No such student found");
+        }
+    }
+}
 
-int countstudents()
+int countdata()
 {
-    
+    printf("%d",(studentcount+1));
+}
+
+void updatedata()
+{
+    printf("Enter Student's Name: ");
+    scanf(" %s", students[studentcount].name);
+    printf("Enter Student's Roll Number: ");
+    scanf("%d", &students[studentcount].rollno);
+    printf("Enter Student's Address: ");
+    scanf(" %[^\n]s", students[studentcount].address);
+    printf("Enter Student's Phone Number: ");
+    scanf("%d", &students[studentcount].phoneno);
+    printf("Enter Student's Marks: ");
+    scanf("%d", &students[studentcount].marks);
+    studentcount++;
+    printf("Student added successfully!\n");
 }
 
 int main(){
@@ -54,21 +89,18 @@ int main(){
                 adddata();
                 break;
             case 2:
-                findStudentByRollNo();
+                finddata();
                 break;
             case 3:
-                findICPMarks();
+                countdata();
                 break;
             case 4:
-                countStudents();
+                deletedata();
                 break;
             case 5:
-                deleteStudent();
+                updatedata();
                 break;
             case 6:
-                updateStudent();
-                break;
-            case 7:
                 printf("Exiting program...\n");
                 return 0;
             default:
